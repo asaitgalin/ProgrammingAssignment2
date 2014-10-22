cc7c05f3f8abc807c2997721d9673bb80e2232fa
Andrey Saitgalin <andrey.saitgalin@gmail.com>
2014-10-22 18:06:42 +0400

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Inverse caching matrix wrapper
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL  # cached inverse
+    
+    # update inner matrix and reset cache
+    setter <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    getter <- function() {
+        return(x)
+    }
+    # update cached inverse
+    inverse_setter <- function(inv) {
+        inverse <<- inv
+    }
+    inverse_getter <- function() {
+        return(inverse)
+    }
+    
+    return(list(set = setter, 
+                get = getter,
+                setinv = inverse_setter,
+                getinv = inverse_getter))
 }
 
-
-## Write a short comment describing this function
-
+# Get inverse of matrix (works with caching matrix wrapper)
+# Returns cached inverse if found, otherwise solves for it and stores to cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinv()
+    # Is inverse cached?
+    if (!is.null(inverse)) {
+        message("getting from cache")
+        return(inverse)
+    }
+    # Solve and update cache
+    m <- x$get()
+    inverse <- solve(m, ...)
+    x$setinv(inverse)
+    return(inverse)
 }
