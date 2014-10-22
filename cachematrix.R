fc1f7dd856bb9d63b5be07b92f2dbca6dbe9fd44
Andrey Saitgalin <andrey.saitgalin@gmail.com>
2014-10-22 18:08:17 +0400

    Refactoring

--- cachematrix.R
+++ cachematrix.R
@@ -28,11 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 # Returns cached inverse if found, otherwise solves for it and stores to cache
 cacheSolve <- function(x, ...) {
     inverse <- x$getinv()
+    
     # Is inverse cached?
     if (!is.null(inverse)) {
         message("getting from cache")
         return(inverse)
     }
+    
     # Solve and update cache
     m <- x$get()
     inverse <- solve(m, ...)
